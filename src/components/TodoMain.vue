ee33cf61549e65b9a5eb5f983f880daa97d08b16
Jaehee0314 <ran_ed@naver.com>
2023-02-09 14:52:13 +0900

    4.삭제기능

--- src/components/TodoMain.vue
+++ src/components/TodoMain.vue
@@ -4,11 +4,17 @@
     <header><h1>Vue Fire todo1</h1></header>
     <main>
       <div class="todos">
-        <div class="write">
+        <div class="write" v-if="writeState === 'add'">
           
           <input ref="writeArea" type="text" v-model = "addItemText" @keyup.enter="addItem"/>
           <button class="btn add" @click="addItem">Add</button>
         </div>
+        <div class="write" v-else> <!--수정-->
+          <input ref="writeArea" type="text" v-model = "editItemText" @keyup.enter="editSave"/>
+          <button class="btn add" @click="editSave">Save</button>
+        </div>
+   
+        
         <ul class="list">
             <li v-for="(todo,i) in todos" :key="todo.text"> 
                 <i
@@ -23,8 +29,8 @@
               {{ todo.text }}
 
               <b>
-                <a href="">Edit</a>
-                <a href="">Del</a>
+                <a href="" @click.prevent="editShow(i)">Edit</a>
+                <a href="" @click.prevent="del(i)">Del</a>
               </b>
             </span>
           </li>
@@ -39,7 +45,10 @@
 export default {
     data() {
         return {
+          writeState:'add',
           addItemText: '',
+          crrEditItem: '',
+          editItemText: '',
             todos:[
                 {text: '공부하기', state: 'yet'}, //yet일때 far
                 {text: '운동하기', state: 'done'}, // fas되야함
@@ -64,8 +73,24 @@ export default {
         else if(this.todos[index].state === 'done')
           this.todos[index].state = 'yet'
 
+      },
+      editShow(index){
+        this.crrEditItem = index; //번호기록
+        this.writeState = 'edit';
+        this.editItemText = this.todos[index].text
 
-      } 
+      },
+      editSave(){ //몇번을 수정할것인지
+        this.todos[this.crrEditItem].text = this.editItemText;
+        this.writeState='add'
+ 
+      },
+      del(index){
+        this.todos.splice(index,1);
+      
+
+
+      }
         
         
 
